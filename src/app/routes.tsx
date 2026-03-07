acc178f5144c631ded4a9dd1a8f03a765052ad75
Yimi Kevin Ponce Rojas-Ing. en Informatica y Sistemas <yimi.ponce@unas.edu.pe>
2026-03-07 14:46:15 -0500

    fix router github pages

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom';
+import { createHashRouter } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
