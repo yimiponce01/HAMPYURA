cefaab26ab4b274ad22de84de8b141873b9df6f4
YimiPonceR <156133329+yimiponce01@users.noreply.github.com>
2026-05-11 13:51:32 -0500

    ,

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/HAMPYURA/',
+  base: '/',
 
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
